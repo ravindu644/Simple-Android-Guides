7551f296e850e99a1f085ceef764502acd2410d9
Ravindu Deshan <Droidcasts@protonmail.com>
2024-02-07 20:46:14 +0530

    Added files

---
+++ nethunter.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+wdir=$(pwd)
+if [ ! -d "$wdir/nethunter" ]; then
+    mkdir nethunter
+    cd nethunter || exit
+    wget "https://raw.githubusercontent.com/ravindu644/android_kernel_apatch_nh_beyondx/Nethunter/nethunter/Kconfig"
+    cd "$wdir" || exit
+fi
+
+if grep -q 'source "nethunter/Kconfig"' "$wdir/arch/arm64/Kconfig"; then
+    echo "Line already exists in arch/arm64/Kconfig. Skipping..."
+else
+    echo 'source "nethunter/Kconfig"' >> "$wdir/arch/arm64/Kconfig"
+    echo "Line added to arch/arm64/Kconfig."
+fi
---
+++ nethunter/README.md
@@ -0,0 +1,6 @@
+## How to Add Nethunter to your Kernel source..?
+
+Enter this command inside the root dir of your kernel source :
+```
+curl -sS https://raw.githubusercontent.com/ravindu644/android_kernel_apatch_nh_beyondx/Nethunter/nethunter.sh | bash
+```
